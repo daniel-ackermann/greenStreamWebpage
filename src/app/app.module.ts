3a7091f0bdc69cbd3f8c9af42f3bb6b98a1d3b86
daniel.ackermann <daniel.ackermann@stud.uni-goettingen.de>
2020-10-21 01:58:53 +0200

    add settings, http interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ListComponent } from './list/list.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LoginComponent, LoginModal } from './login/login.component';
-import { GlobalStatus } from './globalStatus';
+import { LoginComponent } from './login/login.component';
 import { ListItemComponent } from './list-item/list-item.component';
-import { ImportJSONComponent } from './import-json/import-json.component';
 import { FilterPipe } from './filter.pipe';
+import { HttpErrorInterceptor } from './http.interceptor';
+import { LoginService } from './login.service';
+import { LoginRequestService } from './loginRequest.service';
+import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
 
 @NgModule({
     imports: [
@@ -23,18 +25,19 @@ import { FilterPipe } from './filter.pipe';
         ReactiveFormsModule,
         RouterModule.forRoot([
             { path: '', component: ListComponent },
-            { path: 'item', outlet: 'itemModal', loadChildren: () => import('./item/item.module').then(m => m.ItemModule)},
-            { path: 'edit', outlet: 'itemModal', loadChildren: () => import('./item-edit/item-edit.module').then(m => m.ItemEditModule)},
-            { path: 'new', outlet: 'itemModal', loadChildren: () => import('./item-edit/item-edit.module').then(m => m.ItemEditModule)},
-            { path: 'impressum', loadChildren: () => import('./impressum/impressum.module').then(m => m.ImpressumModule)},
-            { path: 'import', loadChildren: () => import('./import-json/import-json.module').then(m => m.ImportJSONModule)},
-            { path: 'login', outlet: 'modal', component:LoginModal },
+            { path: 'item', outlet: 'itemModal', loadChildren: () => import('./item/item.module').then(m => m.ItemModule) },
+            { path: 'edit', outlet: 'itemModal', loadChildren: () => import('./item-edit/item-edit.module').then(m => m.ItemEditModule) },
+            { path: 'new', outlet: 'itemModal', loadChildren: () => import('./item-edit/item-edit.module').then(m => m.ItemEditModule) },
+            { path: 'settings', outlet: 'itemModal', loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule) },
+            { path: 'impressum', loadChildren: () => import('./impressum/impressum.module').then(m => m.ImpressumModule) },
+            { path: 'import', loadChildren: () => import('./import-json/import-json.module').then(m => m.ImportJSONModule) },
+            // { path: 'login', outlet: 'modal', component: LoginModal },
             {
-              path: '**',
-              redirectTo: '',
-              pathMatch: 'full'
+                path: '**',
+                redirectTo: '',
+                pathMatch: 'full'
             }
-        ], { useHash: true })
+        ], { useHash: false, scrollPositionRestoration: 'enabled' })
     ],
     declarations: [
         LoginComponent,
@@ -42,11 +45,18 @@ import { FilterPipe } from './filter.pipe';
         TopBarComponent,
         ListComponent,
         ListItemComponent,
-        ImportJSONComponent,
-        FilterPipe
+        FilterPipe,
+        BottomBarComponent
     ],
     exports: [RouterModule],
-    providers: [GlobalStatus],
-    bootstrap: [ AppComponent ]
+    providers: [
+        LoginService, {
+            provide: HTTP_INTERCEPTORS,
+            useClass: HttpErrorInterceptor,
+            multi: true
+        },
+        LoginRequestService
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
