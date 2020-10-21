c49d490798a4a63827bbb0e847e926bc9b497fdd
daniel.ackermann <daniel.ackermann@stud.uni-goettingen.de>
2020-10-21 02:12:42 +0200

    remove unused stuff

--- src/app/list-item/list-item.component.ts
+++ src/app/list-item/list-item.component.ts
@@ -1,22 +1,17 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit, Input } from '@angular/core';
-import { Item } from 'src/typings';
-import { GlobalStatus } from '../globalStatus';
+import { Component, Input } from '@angular/core';
 import { ItemService } from '../item.service';
 import { ListComponent } from '../list/list.component';
+import { LoginService } from '../login.service';
 
 @Component({
     selector: 'app-list-item',
     templateUrl: './list-item.component.html',
     styleUrls: ['./list-item.component.css']
 })
-export class ListItemComponent implements OnInit {
+export class ListItemComponent {
 
-    constructor(public globalStatus: GlobalStatus, private itemService: ItemService, private itemList: ListComponent) { }
+    constructor(public loginService: LoginService, private itemService: ItemService, private itemList: ListComponent) { }
     @Input() item: any;
-
-    ngOnInit(): void {
-    }
 
     deleteItem(id:number){
         this.itemService.remove(id);
