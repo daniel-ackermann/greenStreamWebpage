d721da9d583d5fd35fa89dd1e9aec7cc7e195411
daniel.ackermann <daniel.ackermann@stud.uni-goettingen.de>
2020-10-24 09:54:43 +0200

    add improved side menu

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { LoginService } from '../login.service';
 import { LoginRequestService } from '../loginRequest.service';
 
 @Component({
-  selector: 'app-top-bar',
-  templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.css']
+    selector: 'app-top-bar',
+    templateUrl: './top-bar.component.html',
+    styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent {
-    constructor(private loginRequestService: LoginRequestService, public loginService: LoginService) {}
-    requestLogin(){
-        this.loginRequestService.requestLogin().catch((err) => {
+    constructor(private loginRequestService: LoginRequestService, public loginService: LoginService, private modalService: NgbModal) { }
+    requestLogin(mode?: number) {
+        this.loginRequestService.requestLogin(mode).catch((err) => {
             console.error(err);
+        });
+    }
+
+    doLogout() {
+        this.loginService.doLogout().then(() => {
+            this.modalService.dismissAll();
+        }).catch((err) => {
+            console.log(err);
         });
     }
 }
