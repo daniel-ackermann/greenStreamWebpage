61fe99204f162141787a76bc0962269753f8eda0
daniel.ackermann <daniel.ackermann@stud.uni-goettingen.de>
2020-10-31 09:53:02 +0100

    improve layout, add username

--- src/app/me/me.component.ts
+++ src/app/me/me.component.ts
@@ -3,6 +3,8 @@ import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 import { LoginService } from '../login.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 
 @Component({
@@ -11,7 +13,6 @@ import { LoginService } from '../login.service';
     styleUrls: ['./me.component.css']
 })
 export class MeComponent {
-    selectedLanguage = new FormControl(this.loginService.user.language);
     languages = [{
         name: "Deutsch",
         value: "de"
@@ -20,36 +21,49 @@ export class MeComponent {
         name: "English",
         value: "en"
     }]
-    form: FormGroup;
+    settings: FormGroup;
 
 
     get ordersFormArray() {
-        return this.form.controls.orders as FormArray;
+        return this.settings.get('selectedLanguage') as FormArray;
     }
 
-    constructor(private formBuilder: FormBuilder, public loginService: LoginService, private location: Location, public router: Router) {
-        this.form = this.formBuilder.group({
-            orders: new FormArray([])
+    constructor(private http: HttpClient, public loginService: LoginService, private location: Location, public router: Router) {
+        this.settings = new FormGroup({
+            username: new FormControl(this.loginService.user.username || ''),
+            selectedLanguage: new FormArray([]),
+            email: new FormControl(this.loginService.user.email),
+            role:  new FormControl(this.loginService.user.role)
         });
         this.addCheckboxes();
     }
 
     private addCheckboxes() {
+        const checkArray: FormArray = this.settings.get('selectedLanguage') as FormArray;
         this.languages.forEach((lang) => {
             if (this.loginService.user.language.split(',').includes(lang.value)) {
-                this.ordersFormArray.push(new FormControl(true))
+                checkArray.push(new FormControl(true))
             } else {
-                this.ordersFormArray.push(new FormControl(false))
+                checkArray.push(new FormControl(false))
             }
         });
     }
 
     submit() {
-        const selectedOrderIds = this.form.value.orders
-            .map((checked, i) => checked ? this.languages[i].value : null)
+        const selectedOrderIds = this.settings.value.selectedLanguage
+            .map((checked:boolean, i:number) => checked ? this.languages[i].value : null)
             .filter(v => v !== null);
 
         this.loginService.setUserLanguage(selectedOrderIds.toString() || "en");
+        this.loginService.setUserName(this.settings.value.username);
+        let user = {
+            username: this.settings.value.username,
+            language: selectedOrderIds.toString(),
+        }
+
+        this.http.put(`${environment.apiMainUrl}/${environment.userPath}/${this.loginService.user.email}`, user).subscribe((data) => {
+            console.log(data);
+        });
         this.location.back();
     }
 
