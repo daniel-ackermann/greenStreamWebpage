67e3de62b7fd27409484508daf980381209ca6c6
daniel.ackermann <daniel.ackermann@stud.uni-goettingen.de>
2021-02-23 23:25:48 +0100

    improve cookie footer

--- src/app/cookie-footer/cookie-footer.component.ts
+++ src/app/cookie-footer/cookie-footer.component.ts
@@ -1,17 +1,34 @@
 import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common'
+import { LoginService } from '../login.service';
 
 @Component({
     templateUrl: './cookie-footer.component.html',
     styleUrls: ['./cookie-footer.component.css']
 })
 export class CookieFooterComponent {
-    cookiesApproved: boolean = false;
+    requestCookies: boolean = true;
+
+
+    constructor(public loginService: LoginService) {
+        this.loginService.onStatusChange.subscribe(status => {
+            if (status === true) {
+                this.acceptCookies();
+            }
+        })
+    }
+
 
     acceptCookies() {
         document.cookie = "acceptCookies=true;same-site=lax";
         console.log("acceptCookies");
-        this.cookiesApproved = true;
+        this.requestCookies = false;
+    }
+
+    rejectCookies() {
+        document.cookie = "acceptCookies=false;same-site=lax";
+        console.log("rejectCookies");
+        this.requestCookies = false;
     }
 }
 
@@ -19,4 +36,4 @@ export class CookieFooterComponent {
     declarations: [CookieFooterComponent],
     imports: [CommonModule]
 })
-class CookieFooterModule {}
+class CookieFooterModule { }
