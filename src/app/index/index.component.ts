1abf8a3b3979fe4f0feb0be6edbdce948bb1fb78
daniel.ackermann <daniel.ackermann@stud.uni-goettingen.de>
2021-03-15 00:04:05 +0100

    improve landing page

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -8,8 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class IndexComponent {
 
     constructor() { }
-    images = [944, 1020, 984].map((n) => `https://picsum.photos/id/${n}/1200/500`);
-
+    items = [
+        {
+            image: `https://appsterdb.ackermann.digital/assets/greenwashing2.jpg`,
+            title: "Greenwashing",
+            description: "Darum ist Greenwashing ein Problem und schadet uns allen!",
+            link: "https://www.quarks.de/umwelt/klimawandel/darum-ist-greenwashing-ein-problem/"
+        },
+        {
+            image: `https://appsterdb.ackermann.digital/assets/earth.jpg`,
+            title: "Podcast Climaware",
+            description: "Die erste Folge rund um Energiebilanz, den Treibhauseffekt und das Erdsystem!",
+            link: "https://climaware.fireside.fm/klima-hintergrundwissen1?t=0"
+        }
+    ]
 }
 
 
