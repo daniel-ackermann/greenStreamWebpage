69087de41404428542209605e478c239cd6e4963
daniel.ackermann <daniel.ackermann@stud.uni-goettingen.de>
2021-03-23 09:48:48 +0100

    refactor: move logic to homeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,151 @@
 import { Component, OnInit } from '@angular/core';
-import { LoginService } from '../login.service';
+import { ItemService } from 'src/app/item.service';
+import { Category, Item, Language, Topic } from "src/typings";
+import { LoginService } from 'src/app/login.service';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { LoginRequestService } from 'src/app/loginRequest.service';
+import { ActivatedRoute, ActivationStart, NavigationStart, Router, RouterEvent } from '@angular/router';
 
 @Component({
-    selector: 'app-home',
+    selector: 'app-list',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+    searchText = "";
+    items: Item[] = [];
+    loading: boolean = true;
+    showNoItemHint: boolean = false;
+    moreItemsAvailable: boolean = true;
+    topics: Topic[] = [];
+    selection: FormGroup;
+    selected: string = "all";
+    // I would prefer an array, but I did not find any other working solution.
+    // Problem was to get the array information by the pattern in the uri. I do want words and not numbers there.
+    categories = {
+        "all": {
+            requiresAuth: false,
+            pattern: "all",
+            name: "Browse",
+            url: environment.itemsPath,
+            title: "Overview"
+        },
+        "created": {
+            requiresAuth: true,
+            pattern: "created",
+            name: "Created",
+            url: environment.createdItemsPath,
+            title: "Created"
+        },
+        "liked": {
+            requiresAuth: true,
+            pattern: "liked",
+            name: "Liked",
+            url: environment.likedItemsPath,
+            title: "Liked"
+        },
+        "history": {
+            requiresAuth: true,
+            pattern: "history",
+            name: "History",
+            url: environment.watchedItemsPath,
+            title: "History"
+        },
+        "reviewed": {
+            requiresAuth: true,
+            pattern: "reviewed",
+            name: "Reviewed",
+            url: environment.reviewedItemsPath,
+            title: "Reviewed"
+        },
+        "review": {
+            requiresAuth: true,
+            pattern: "review",
+            name: "Review",
+            url: environment.reviewItemsPath,
+            title: "Review"
+        }
+    };
+    
+    constructor(public itemService: ItemService,
+        public loginService: LoginService,
+        private loginRequestService: LoginRequestService,
+        private http: HttpClient,
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router) {
+        
+        // topic form 
+        this.selection = this.formBuilder.group({
+            topics: new FormArray([])
+        });
 
-    constructor(public loginService: LoginService) { }
+        // load category according to route
+        const foo = this.route.snapshot.paramMap.get('topic');
+        this.switchView(this.categories[foo]);
 
-    ngOnInit(): void { }
+        // switch page on logout
+        this.loginService.onStatusChange.subscribe((loggedIn) => {
+            if(loggedIn === false){
+                this.switchView(this.categories["all"]);
+            }
+        });
+        this.router.events.subscribe((event: RouterEvent) => {
+            if(event instanceof ActivationStart && event.snapshot.params.topic){
+                this.switchView(this.categories[event.snapshot.params.topic]);
+            }
+        });
+    }
+
+    get selectionFormArray() {
+        return this.selection.get('topics') as FormArray;
+    }
+
+    get categoryArray(){
+        return Object.values(this.categories);
+    }
+
+
+    get selectedTopicIds(){
+        return this.selection.value.topics.map(topic => topic.selected ? topic.id : null).filter(x => x !== null);
+    }
+
+    ngOnInit() {
+        // load topics into form
+        this.http.get<Topic[]>(`${environment.apiMainUrl}/${environment.topicsPath}`).subscribe((data) => {
+            this.topics = data;
+            data.forEach((topic) => {
+                topic.selected = false;
+                this.selectionFormArray.push(this.formBuilder.group(topic));
+            });
+        });
+    }
+
+    // load category into view, request login or redirect
+    switchView(index: Category) {
+        this.selected = index.pattern;
+        this.itemService.load(index.url, this.selectedTopicIds, 10, 0).subscribe({
+            error:(err) => {
+                console.log(err);
+                if(index.requiresAuth && !this.loginService.isLoggedIn){
+                    return this.loginRequestService.requestLogin().then((user) => {
+                        this.switchView(index)
+                    }).catch(() => {
+                        this.switchView(this.categories["all"])
+                    });
+                }
+            }
+        }); 
+    }
+
+    loadCategory(limit: number = 20, index: number = 0) {
+        this.items = [];
+        this.itemService.load(this.itemService.loadedTopic, this.selectedTopicIds, limit, index).subscribe();
+    }
+
+    submit() {
+        this.loadCategory(20, 0);
+    }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,16 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
-import { ListComponent } from './list/list.component';
-import { CreatedComponent } from './created/created.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ReviewComponent } from './review/review.component';
-import { ReviewedComponent } from './reviewed/reviewed.component';
 import { ListItemComponent } from './list-item/list-item.component';
 import { FilterPipe } from './list-item/filter.pipe';
-import { LikedComponent } from './liked/liked.component';
-import { WatchedComponent } from './watched/watched.component';
-import { WatchlistComponent } from './watchlist/watchlist.component';
 import { SharedModule } from '../shared/shared.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -20,15 +13,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
     declarations: [
         HomeComponent,
-        ListComponent,
-        CreatedComponent,
-        ReviewComponent,
-        ReviewedComponent,
         ListItemComponent,
-        FilterPipe,
-        LikedComponent,
-        WatchedComponent,
-        WatchlistComponent
+        FilterPipe
     ],
     imports: [
         SharedModule,
@@ -40,47 +26,17 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
             { path: 'item', outlet: 'itemModal', loadChildren: () => import('./item/item.module').then(m => m.ItemModule) },
             { path: 'edit', outlet: 'itemModal', loadChildren: () => import('../item-edit/item-edit.module').then(m => m.ItemEditModule) },
             {
-                path: '',
+                path: ':topic',
                 component: HomeComponent,
-                children: [
-                    {
-                        path: 'reviewed',
-                        component: ReviewedComponent
-                    },
-                    {
-                        path: 'created',
-                        component: CreatedComponent
-                    },
-                    {
-                        path: 'liked',
-                        component: LikedComponent
-                    },
-                    {
-                        path: 'history',
-                        component: WatchedComponent
-                    },
-                    {
-                        path: 'watchlist',
-                        component: WatchlistComponent
-                    },
-                    {
-                        path: 'review',
-                        component: ReviewComponent
-                    },
-                    {
-                        path: 'all',
-                        component: ListComponent
-                    },
-                    {
-                        path: '**',
-                        redirectTo: 'all',
-                        pathMatch: 'full'
-                    }
-                ],
+            },
+            {
+                path: '**',
+                redirectTo: 'all',
+                pathMatch: 'full'
             }
         ])
     ],
-    bootstrap: [HomeComponent],
-    exports: [HomeComponent]
+    bootstrap: [],
+    exports: []
 })
 export class HomeModule { }
