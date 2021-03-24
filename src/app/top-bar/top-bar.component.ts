90cf87403a7611a3763c7aa648ae03fca81206cf
daniel.ackermann <daniel.ackermann@stud.uni-goettingen.de>
2021-03-24 23:29:15 +0100

    feat: navigate on logout

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { LoginService } from '../login.service';
 import { LoginRequestService } from '../loginRequest.service';
@@ -10,7 +10,7 @@ import { LoginRequestService } from '../loginRequest.service';
     styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent {
-    constructor(private loginRequestService: LoginRequestService, public loginService: LoginService, private modalService: NgbModal) { }
+    constructor(private loginRequestService: LoginRequestService, public loginService: LoginService, private modalService: NgbModal, private router: Router) { }
     collapsed: boolean = true;
     requestLogin(mode?: number) {
         this.loginRequestService.requestLogin(mode).catch((err) => {
@@ -20,6 +20,7 @@ export class TopBarComponent {
 
     doLogout() {
         this.loginService.doLogout().then(() => {
+            this.router.navigate(["list", "all"]);
             this.modalService.dismissAll();
         }).catch((err) => {
             console.log(err);
