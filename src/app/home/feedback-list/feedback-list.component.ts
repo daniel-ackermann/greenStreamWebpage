0ef64a4ea64ce67fc19bc20e58a71019f2e51fe5
daniel.ackermann <daniel.ackermann@stud.uni-goettingen.de>
2021-03-24 23:30:57 +0100

    approv: delete feedback, design, auth

--- src/app/home/feedback-list/feedback-list.component.ts
+++ src/app/home/feedback-list/feedback-list.component.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { LoginService } from 'src/app/login.service';
+import { LoginRequestService } from 'src/app/loginRequest.service';
 import { environment } from 'src/environments/environment';
 import { Feedback } from 'src/typings';
 
@@ -16,23 +18,49 @@ export class FeedbackListComponent implements OnInit {
     searchText: string = "";
     loading: boolean = true;
     moreAvailable: boolean = true;
-    constructor(private http: HttpClient, public loginService: LoginService) { }
+    constructor(private http: HttpClient,
+        public loginService: LoginService,
+        private loginRequestService: LoginRequestService,
+        private router: Router) { }
 
     ngOnInit(): void {
         this.load([], 20, 0);
     }
 
     loadMore(topics: number[] = [], limit: number = 10, start: number = 0) {
-        this.loadFeedbacks(topics, limit, start).subscribe((data: Feedback[]) => {
-            this.feedbacks = this.feedbacks.concat(data);
+        this.loadFeedbacks(topics, limit, start).subscribe({
+            next: (data:Feedback[]) => {
+                this.feedbacks.concat(data);
+            },
+            error:(err) => {
+                console.log(err);
+                if(!this.loginService.isLoggedIn){
+                    return this.loginRequestService.requestLogin().then((user) => {
+                        this.loadMore(topics, limit, start)
+                    }).catch(() => {
+                        this.router.navigate(['list', 'all']);
+                    });
+                }
+            }
         });
     }
 
     load(topics: number[] = [], limit: number = 10, start: number = 0) {
         console.log("FeedbackService.load");
-        this.loadFeedbacks(topics, limit, start).subscribe((data: Feedback[]) => {
-            console.log(data);
-            this.feedbacks = data;
+        this.loadFeedbacks(topics, limit, start).subscribe({
+            next: (data: Feedback[])=> {
+                this.feedbacks = data;
+            },
+            error: (err) => {
+                console.log(err);
+                if(!this.loginService.isLoggedIn){
+                    return this.loginRequestService.requestLogin().then((user) => {
+                        this.load(topics, limit, start)
+                    }).catch(() => {
+                        this.router.navigate(['list', 'all']);
+                    });
+                }
+            }
         });
     }
 
@@ -56,5 +84,10 @@ export class FeedbackListComponent implements OnInit {
                 return data;
             })
         );
+    }
+
+    deleteFeedback(id: number, index: number) {
+        this.http.delete(`${environment.apiMainUrl}/${environment.deleteFeedbackPath}/${id}`).subscribe();
+        this.feedbacks.splice(index, 1);
     }
 }
