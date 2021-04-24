495a150ac556b115f052345b7e2822389d21bbe0
daniel.ackermann <daniel.ackermann@stud.uni-goettingen.de>
2021-04-24 17:27:03 +0200

    feat: update index page

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -8,24 +8,66 @@ import { Component, OnInit } from '@angular/core';
 export class IndexComponent {
 
     constructor() { }
-    items = [
+    items = [{
+        image: "assets/index/Was_kann_ich_tun.png",
+        title: "Was kann ich tun?",
+        description: "Du möchtest dein Klimawissen in Handlung umsetzen, weißt aber nicht wie? Dann höre unbedingt in den Podcast mit Harald Lesch rein!",
+        link: "https://climaware.fireside.fm/haraldlesch"
+    },
+
+    {
+        image: "assets/index/Gefahren_Klimawandel.png",
+        title: "Gefahren durch den Klimawandel",
+        description: "Was passiert, wenn wir den Klimawandel nicht stoppen? - Quarks",
+        link: "https://www.youtube.com/watch?v=FoMzyF_B7Bg"
+    },
+
+    {
+        image: "assets/index/Was_Klimawandel.png",
+        title: "Was ist der Klimawandel?",
+        description: "Das Klimawandel-FAQ vom Umweltbundesamt",
+        link: "https://www.umweltbundesamt.de/themen/klima-energie/klimawandel/haeufige-fragen-klimawandel#1-was-ist-eigentlich-klima"
+    }];
+
+    klimawandel = [{
+        image: "Status_Quo.png",
+        title: "Status Quo des Klimawandels",
+        description: "Die dritte Folge des Podcasts Climaware um die bisherigen Klima-Veränderungen laut IPCC",
+        link: "https://climaware.fireside.fm/ipcc-wissen"
+    },
+
+    {
+        image: "Wissenschaft_Klimawissen.png",
+        title: "Klimwandel - Was die Wissenschaft wirklich weiß",
+        description: "Der erste Teil der Doku zum Klimawandel von maiLab (WDR)",
+        link: "https://www.youtube.com/watch?v=oJ1zm65u-ck"
+    },
+
+    {
+        image: "Fakten_Klimawandel.jpg",
+        title: "Fakten zum Klimawandel",
+        description: "24 Fakten zum Klimawandel der Scientists for future",
+        link: "https://de.scientists4future.org/ueber-uns/stellungnahme/fakten/"
+    }]
+    tun = [
         {
-            image: `https://appsterdb.ackermann.digital/assets/greenwashing2.jpg`,
-            title: "Greenwashing",
-            description: "Darum ist Greenwashing ein Problem und schadet uns allen!",
-            link: "https://www.quarks.de/umwelt/klimawandel/darum-ist-greenwashing-ein-problem/"
+            image: "Klimawandel_tun.png",
+            title: "Klimawandel - Was wir tatsächlich tun können",
+            description: "Der zweite Teil der Doku zum Klimawandel von maiLab (WDR)",
+            link: "https://www.youtube.com/watch?v=bCvUwnIdqBI"
         },
         {
-            image: `https://appsterdb.ackermann.digital/assets/earth.jpg`,
-            title: "Podcast Climaware",
-            description: "Die erste Folge rund um Energiebilanz, den Treibhauseffekt und das Erdsystem!",
-            link: "https://climaware.fireside.fm/klima-hintergrundwissen1?t=0"
+            image: "Was_kann_ich_tun.jpg",
+            title: "Was kann ich tun?",
+            description: "Du möchtest dein Klimawissen in Handlung umsetzen, weißt aber nicht wie? Dann höre unbedingt in den Podcast mit Harald Lesch rein!",
+            link: "https://climaware.fireside.fm/haraldlesch"
+        },
+
+        {
+            image: "Umwelttipps.jpg",
+            title: "Umwelttipps für den Alltag vom Umweltbundesamt",
+            description: "Elektrogeräte, Essen & Trinken, Garten & Freizeit, Haushalt & Wohnen, Heizen & Bauen, Mobilität",
+            link: "https://www.umweltbundesamt.de/umwelttipps-fuer-den-alltag"
         }
     ]
 }
-
-
-// @Component({selector: 'ngbd-carousel-basic', templateUrl: './carousel-basic.html'})
-// export class NgbdCarouselBasic {
-
-// }
